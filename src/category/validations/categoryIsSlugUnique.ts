5340c797a19f06a024b8741d57bbd4078d42f9bc
philippeberto <philippeberto@outlook.com>
2021-05-23 11:43:31 +0100

    bug fix to update new category with same slug then old category

--- src/category/validations/categoryIsSlugUnique.ts
+++ src/category/validations/categoryIsSlugUnique.ts
@@ -6,8 +6,13 @@ export class CategoryIsSlugUnique implements ValidatorConstraintInterface {
   constructor(private readonly categoryService: CategoryService) {
 
   }
-  async validate(text: string, validationArguments): Promise<boolean> {
+  async validate(text: string, validationArguments: ValidationArguments): Promise<boolean> {
     const category = await this.categoryService.findBySlug(text)
+    console.log(validationArguments.object['id'])
+    //check if operatoin is an update
+    if (validationArguments.object['id'] && validationArguments.object['id'] === category.id) {
+      return true
+    }
     return category ? false : true
   }
   defaultMessage(args: ValidationArguments): string {
